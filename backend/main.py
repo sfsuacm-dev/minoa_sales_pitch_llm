2fc04928095adad65171981c8d32c1841e6b259a
DiscoDoggy <akaattarilim@gmail.com>
2024-11-12 21:54:37 -0800

    aded cors hot fix

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from .routes.generation.controller import router as generation_router
 from .routes.sources.controller import router as sources_router
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+
+)
 
 app.include_router(generation_router)
 app.include_router(sources_router)
