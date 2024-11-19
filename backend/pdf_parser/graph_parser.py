ba10a29adf64a12ca8cd8a5b961e6ff208388324
DiscoDoggy <akaattarilim@gmail.com>
2024-11-18 23:34:31 -0800

    Implemented functionality to loop through CLI inputted directory to store PDF text chunks inside a unified CSV

--- backend/pdf_parser/graph_parser.py
+++ backend/pdf_parser/graph_parser.py
@@ -1,11 +1,23 @@
 import PyPDF2
 import csv
 import re
+import os
+import argparse
+from ..dependencies.firebase_interface import FirestoreWorker
 
 def extract_title(text):
     lines = text.split('\n')
-    
-    return ' '.join([lines[1].strip(), lines[2].strip()]).strip() if lines else 'Untitled Document'
+    title = ""
+    for i in range(len(lines)):
+        if i == 0:
+            continue
+        
+        if i > 1:
+            title+=" "
+
+        title += lines[i].strip()
+    return title
+    # return ' '.join([lines[1].strip(), lines[2].strip()]).strip() if lines else 'Untitled Document'
 
 def extract_description(text):
     match = re.search(r'Description:(.*?)(?:Read more|Note\(s\):)', text, re.DOTALL)
@@ -21,9 +33,9 @@ def clean_pdf_to_csv(pdf_path, csv_path):
         first_page = reader.pages[0]
         document_title = extract_title(first_page.extract_text())
         
-        with open(csv_path, 'w', newline='', encoding='utf-8') as csvfile:
+        with open(csv_path, mode='a', newline='', encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile)
-            writer.writerow(['Title', 'Description'])
+            # writer.writerow(['Title', 'Description'])
             
             for page in reader.pages:
                 text = page.extract_text()
@@ -31,7 +43,28 @@ def clean_pdf_to_csv(pdf_path, csv_path):
                 if description:
                     writer.writerow([document_title, description])
 
-# Usage
-pdf_path = 'graph.pdf' # Change to our file path before running
-csv_path = 'csvcleanedpdf.csv' # does not append to existing file
-clean_pdf_to_csv(pdf_path, csv_path)
+def ingest_data():
+    pass
+
+
+def main():
+    # pdf_path = 'graph.pdf' # Change to our file path before running
+    # csv_path = './results.csv' # does not append to existing file
+    # clean_pdf_to_csv(pdf_path, csv_path)
+    parser = argparse.ArgumentParser(description="Process PDFs in inputted directory")
+    parser.add_argument("--directory_name", type=str, required=True, help="Directory with PDFs")
+    
+    results_csv_path = "./results.csv"
+    with open(results_csv_path, mode="a", newline="", encoding="utf-8") as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["Title", "Chunk"])
+
+    args = parser.parse_args()
+    folder_path = args.directory_name
+    for file in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file)
+        print(f"parsing file: {file}...")
+        clean_pdf_to_csv(file_path, results_csv_path)
+
+if __name__ == "__main__":
+    main()
