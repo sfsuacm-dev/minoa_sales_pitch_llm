f5955d922f662a7328eb9e9afdaae4bf2dafef2d
RussianGravy <valentinejones1215@gmail.com>
2024-12-09 09:58:43 -0800

    add unselect function

--- frontend/react-front/src/pages/SourcesPage.jsx
+++ frontend/react-front/src/pages/SourcesPage.jsx
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import CheckMark from "../images/image.png";
 import { useRequestContext } from "../contexts/request_context";
 import styles from "./InputsPage.module.css";
+import Navbar from "../components/Navbar";
 
 export default function SourcesPage() {
   const navigate = useNavigate(); //for redirecting to other pages
@@ -11,19 +12,6 @@ export default function SourcesPage() {
   const [modal, setModal] = useState({ title: "", content: "" });
   var selectedSources = []; //the sources submitted with the request
   const [sources, setSources] = useState([]);
-  /*
-    GET sources/all_sources
-    REQUEST MODEL
-    NONE
-
-    RESPONSE MODEL
-    [
-      {
-        "source_id": 0,
-        "source_name": "string"
-      }
-    ]
-  */
 
   useEffect(() => {
     const fetchAndStore = async () => {
@@ -52,51 +40,7 @@ export default function SourcesPage() {
       className="w-screen h-screen flex flex-col"
       style={{ backgroundColor: "#D5FBE6" }}
     >
-      <nav className={styles.nav}>
-        <div className={styles.logo}>
-          <svg
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="M12 2L2 7L12 12L22 7L12 2Z"
-              stroke="#0f172a"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M2 17L12 22L22 17"
-              stroke="#0f172a"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-            <path
-              d="M2 12L12 17L22 12"
-              stroke="#0f172a"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-            />
-          </svg>
-          Minoa
-        </div>
-        <div className={styles.navLinks}>
-          <a href="/">Home</a>
-          <a href="/">Product</a>
-          <a href="/">Resources</a>
-          <a href="/">Careers</a>
-          <a href="/">About</a>
-          <a href="/" className={`${styles.button} ${styles.buttonOutline}`}>
-            Login
-          </a>
-          <a href="/" className={`${styles.button} ${styles.buttonFilled}`}>
-            Book a demo
-          </a>
-        </div>
-      </nav>
+      <Navbar />
       {toggle ? (
         <Modal
           onClose={() => {
@@ -135,8 +79,8 @@ export default function SourcesPage() {
                 onSelect={(x) => {
                   if (x) selectedSources.push(source.source_id);
                   else {
-                    const index = selectedSources.indexOf(x);
-                    // index > 0 ? selectedSources.slice() : "";
+                    const index = selectedSources.indexOf(source.source_id);
+                    selectedSources.splice(index, 1);
                   }
                   console.log(selectedSources);
                 }}
